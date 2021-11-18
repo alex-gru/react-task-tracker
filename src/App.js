0b3b1087fb0ac1f18b2cd36d8a650aa002da6655
Alexander Gruschina <gruschina@symptoma.com>
2021-11-18 14:30:43 +0100

    task data in JSON, loop over list, delete tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="container">
-        <Header title='Hello' />
-    </div>
-  );
+    const [tasks, setTasks] = useState([
+            {
+                id: 1,
+                text: 'Doctors appointment',
+                day: 'Today',
+                reminder: false
+            },
+            {
+                id: 2,
+                text: 'Pick up girlfriend',
+                day: 'Tomorrow',
+                reminder: false
+            }
+        ]
+    )
+
+    // Delete Task
+    const deleteTask = (id) => {
+        console.log('delete', id)
+        setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    return (
+        <div className="container">
+            <Header/>
+            {tasks.length > 0 ?
+                (
+                    <Tasks tasks={tasks} onDelete={deleteTask}/>
+                ) : <p>No tasks available.</p>
+            }
+        </div>
+    );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({ title }) => (
-    <header>
-        <h1>{title}</h1>
-    </header>
-);
+const Header = ({ title }) => {
+    const onClick = (e) => {
+      console.log('Click')
+    }
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick}/>
+        </header>
+    );
+};
 
 Header.defaultProps = {
     title: 'Task Tracker'
@@ -13,5 +20,10 @@ Header.defaultProps = {
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red'
+// }
 
 export default Header;
